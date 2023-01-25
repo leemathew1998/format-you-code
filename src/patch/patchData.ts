d031acc8e450e09bea5c6ae86e2ac234245a0c87
leemathew1998 <16408400236@stu.hut.edu.cn>
2023-01-25 15:49:01 +0800

    ✨ 完成process methods部分

--- src/patch/patchData.ts
+++ src/patch/patchData.ts
@@ -20,13 +20,14 @@ const patchData = (moduleLines: any, hasModules: any, renderFunc: any) => {
       trueEndIndex: startIndex + hasModules[key].length - 1,
     };
     if (key === "data") {
-      processName(moduleLines, range, renderFunc);
+      processData(moduleLines, range, renderFunc);
+    } else if (key === "methods") {
+      processMethods(moduleLines, range, renderFunc);
     }
-    console.log(moduleLines);
   });
 };
 
-const processName = (moduleLines, range, renderFunc) => {
+const processData = (moduleLines, range, renderFunc) => {
   const needFixVariable = moduleLines.splice(
     range.trueStartIndex,
     range.trueEndIndex - range.trueStartIndex + 1
@@ -56,6 +57,78 @@ const processName = (moduleLines, range, renderFunc) => {
       firstLineNumber++;
       delete item.thisVarIndex;
     }
+  });
+  moduleLines.splice(range.trueStartIndex, 0, ...needFixVariable);
+};
+const processMethods = (moduleLines, range, renderFunc) => {
+  const needFixVariable = moduleLines.splice(
+    range.trueStartIndex,
+    range.trueEndIndex - range.trueStartIndex + 1
+  );
+  let firstLineNumber = -1;
+  let stackForComma: string[] = [];
+  let currentIndex = 999999;
+  for (let index = 0; index < needFixVariable.length; index++) {
+    const item = needFixVariable[index];
+    item.thisVarIndex = currentIndex;
+    if (firstLineNumber === -1) {
+      firstLineNumber = item.lineNumber;
+    }
+    if (!item.text.trim()) continue;
+    //把空格删掉
+    item.textCopy = item.text.replace(/\s/g, "");
+
+    if (item.textCopy.indexOf("methods:{") !== -1) {
+      item.thisVarIndex = -1000;
+    } else if (
+      (item.textCopy.indexOf("}") !== -1 ||
+        item.textCopy.indexOf("},") !== -1) &&
+      index === needFixVariable.length - 1
+    ) {
+      item.thisVarIndex = 1000000;
+    }
+    let variableName = item.textCopy.match(/(\w+)\((\w+)?\)\{/);
+
+    if (item.textCopy.indexOf("}") !== -1 && stackForComma.length) {
+      stackForComma.pop();
+    }
+    //检测xxx(){或者xxx(...){两种情况，一种有参数，一种没有参数
+
+    if (!variableName) {
+      if (
+        item.textCopy.indexOf("},") !== -1 ||
+        item.textCopy.indexOf("}") !== -1
+      ) {
+        currentIndex = 999999;
+      }
+      continue;
+    }
+
+    //此处需要看这一行是不是这个函数的最后一行，检测{和}字符，存到stackForComma中
+    if (item.textCopy.indexOf("{") !== -1) {
+      stackForComma.push("{");
+    }
+
+    variableName = variableName[1];
+    const reg = new RegExp(`\\b${variableName}\\b`, "g");
+    const isshow = renderFunc.match(reg);
+    if (!isshow) {
+      currentIndex = 999999;
+      continue;
+    }
+    const thisVarIndex = renderFunc.indexOf(isshow[0]);
+    item.thisVarIndex = thisVarIndex;
+    currentIndex = thisVarIndex;
+  }
+
+
+  needFixVariable.sort((a, b) => a?.thisVarIndex - b?.thisVarIndex);
+
+  needFixVariable.forEach((item) => {
+    item.lineNumber = firstLineNumber;
+    firstLineNumber++;
+    delete item.textCopy;
+    delete item.thisVarIndex;
   });
   moduleLines.splice(range.trueStartIndex, 0, ...needFixVariable);
 };
